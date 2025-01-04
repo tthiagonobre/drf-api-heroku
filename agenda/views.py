8a83d2211140b4d2952cd1edba0631a0bef58097
Thiago Nobre <thiagocardoso.nobre@gmail.com>
2025-01-04 16:30:25 -0400

    Remover arquivos __pycache__ do controle de versão

--- agenda/views.py
+++ agenda/views.py
@@ -1,11 +1,20 @@
 from django.http import JsonResponse
 from django.shortcuts import get_object_or_404
+from rest_framework.decorators import api_view
 
 from agenda.models import Agendamento
 from agenda.serializers import AgendamentoSerializer
 
 # Create your views here.
+@api_view(http_method_names=["GET"]) #api que retorna objs de JSON
 def agendamento_detail(request, id):
    obj = get_object_or_404(Agendamento, id=id)
    serializer = AgendamentoSerializer(obj)
    return JsonResponse(serializer.data) 
+
+@api_view(http_method_names=["GET"])
+def agendamento_list(request):
+   qs = Agendamento.objects.all()
+   serializer = AgendamentoSerializer(qs, many=True) #many=True -> serializar muitos obj
+   return JsonResponse(serializer.data, safe=False) 
+   
--- tamarcado/urls.py
+++ tamarcado/urls.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
-from django.urls import path
-from django.conf.urls import include
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('agenda.urls'))
+    path('api/', include('agenda.urls')),
 ]
