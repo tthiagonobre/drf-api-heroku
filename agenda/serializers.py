904089e92168e9cd14aa62670fa75b5185c2aa59
Thiago Nobre <thiagocardoso.nobre@gmail.com>
2025-01-07 18:51:19 -0400

    Refatoração de código

--- agenda/serializers.py
+++ agenda/serializers.py
@@ -1,7 +1,26 @@
+from django.utils.timezone import now
 from rest_framework import serializers
 
-class AgendamentoSerializer(serializers.Serializer):
-   data_horario = serializers.DateTimeField()
-   nome_cliente = serializers.CharField(max_length=200)
-   email_cliente = serializers.EmailField()
-   telefone_cliente = serializers.CharField(max_length=20)
+from agenda.models import Agendamento
+
+class AgendamentoSerializer(serializers.ModelSerializer):
+   class Meta:
+      model = Agendamento
+      fields = ["id", "data_horario", "nome_cliente", "email_cliente", "telefone_cliente"]
+   
+   
+   def validate_data_horario(self, value):
+      if value < now():
+         raise serializers.ValidationError("Agendamento não pode ser feito no passado!")
+      return value
+   
+   
+   def validate(self, attrs):
+      telefone_cliente = attrs.get("telefone_cliente", "")
+      email_cliente = attrs.get("email_cliente", "")
+      
+      if email_cliente.endswith(".br") and telefone_cliente.startswith("+") and not telefone_cliente.startswith("+55"):
+         raise serializers.ValidationError("Email brasileiro deve estar associado a um número de telefone do Brasil (+55).")
+      return attrs
+   
+ 
--- agenda/views.py
+++ agenda/views.py
@@ -9,25 +9,21 @@ from agenda.serializers import AgendamentoSerializer
 # Create your views here.
 @api_view(http_method_names=["GET", "PATCH", "POST"]) #api que retorna objs de JSON
 def agendamento_detail(request, id):
+   obj = get_object_or_404(Agendamento, id=id)
    if request.method == "GET":
-      obj = get_object_or_404(Agendamento, id=id)
       serializer = AgendamentoSerializer(obj)
       return JsonResponse(serializer.data) 
+   
    if request.method == "PATCH":
-      obj = get_object_or_404(Agendamento, id=id)
-      serializer = AgendamentoSerializer(data=request.data, partial=True)#partial=True -> Aceita updates parciais
+
+      serializer = AgendamentoSerializer(obj, data=request.data, partial=True)#partial=True -> Aceita updates parciais
       if serializer.is_valid():
-         validated_data = serializer.validated_data
-         # Alterar cada atributo presente em validated_data
-         obj.data_horario = validated_data.get("data_horario", obj.data_horario)
-         obj.nome_cliente = validated_data.get("nome_cliente", obj.nome_cliente)
-         obj.email_cliente = validated_data.get("email_cliente", obj.email_cliente)
-         obj.telefone_cliente = validated_data.get("telefone_cliente", obj.telefone_cliente)
-         obj.save()
-         return JsonResponse(validated_data, status=200)#success
+         serializer.save()    
+         return JsonResponse(serializer.data, status=200)#success
       return JsonResponse(serializer.errors, status=400)#bad request
+   
    if request.method == "POST":
-      obj = get_object_or_404(Agendamento, id=id)
+      
       if obj.is_canceled:
          return JsonResponse({"detail": "Agendamento já foi cancelado"}, status=400) # Evita recancelamento
       obj.is_canceled = True
@@ -59,16 +55,10 @@ def agendamento_list(request):
       data = request.data
       serializer = AgendamentoSerializer(data=data)
       if serializer.is_valid():
-         validated_data = serializer.validated_data
-         Agendamento.objects.create(
-               data_horario=validated_data["data_horario"],
-               nome_cliente=validated_data["nome_cliente"],
-               email_cliente=validated_data["email_cliente"],
-               telefone_cliente=validated_data["telefone_cliente"],
-         )
+         serializer.save()
          return JsonResponse(serializer.data, status=201)
       return JsonResponse(serializer.errors, status=400)
 
    
       
-#f086
+#f089
