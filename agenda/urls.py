486eb8318fed44a07f6e6f0ac38b9a6318176374
Thiago Nobre <thiagocardoso.nobre@gmail.com>
2025-03-11 23:54:35 -0400

    Salvando alterações antes do pull

--- agenda/urls.py
+++ agenda/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from agenda.views import AgendamentoDetail, AgendamentoList, get_relatorio_prestadores, get_horarios, healthcheck
+from agenda.views import AgendamentoDetail, AgendamentoList, PrestadorList, get_horarios, healthcheck
 
 urlpatterns = [
    path('agendamentos/', AgendamentoList.as_view()),
    path('agendamentos/<int:pk>/', AgendamentoDetail.as_view(), name='agendamento_detail'),
    path('horarios/', get_horarios),
-   path('prestadores/', get_relatorio_prestadores),
+   path('prestadores/', PrestadorList.as_view()),
    path('', healthcheck),
 ]
